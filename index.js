a7d1b77bd24fd18add693413967e76ebf19f211c
sabuj750 <sabuj712515@gmail.com>
2025-06-16 20:27:43 +0530

    delete and edit and final testing

--- index.js
+++ index.js
@@ -68,6 +68,40 @@ app.delete("/delete/:id", async (req , res) => {
   }
 });
 
+app.get('/edit/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+    if(result.rows.length > 0){
+      const book = result.rows[0];
+      console.log("Book found:", book);
+      res.render("new.ejs", { book });
+    }else{
+      res.status(404).send("Book not found");
+    }
+  }catch(error){
+    console.error("Error fetching book:", error);
+  }
+});
+
+app.post('/edit/:id', async (req, res) => {
+  try{
+    const id = req.params.id;
+    const details = req.body;
+    const title = details.title;
+    const author = details.author;
+    const review = details.review;
+    const rating = details.rating;
+    const date_read = details.date_read;
+
+    db.query("UPDATE books SET title = $1 , author = $2 , rating = $3 , review = $4 , date_read = $5 WHERE id = $6",[title, author, rating, review, date_read, id]);
+    res.redirect("/");
+  }catch(error){
+    console.error("Error updating book:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
