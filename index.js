679bbaf5343f97033079081deebf02bae19dce97
sabuj750 <sabuj712515@gmail.com>
2025-06-16 16:05:36 +0530

    setting the post and get route

--- index.js
+++ index.js
@@ -32,6 +32,29 @@ app.get('/add', (req , res) => {
   res.render("new.ejs");
 });
 
+app.post("/add" , async (req , res) => {
+  try{
+    const details = req.body;
+    const title = details.title;
+    const author = details.author;
+    const review = details.review;
+    const rating = details.rating;
+    const date_read = details.date_read;
+
+    const response = await axios.get(`http://openlibrary.org/search.json?title=${title}`);
+    const coverId = response.data.docs[0]?.cover_i;
+    const cover_Url = coverId ? `https://covers.openlibrary.org/b/id/${coverId}-L.jpg` : null;
+
+    await db.query("INSERT INTO books (title , author, cover_url , rating , review , date_read ) VALUES($1 , $2 , $3 , $4 , $5 , $6)",[title, author, cover_Url , rating , review , date_read]);
+
+    res.redirect("/");
+
+  }catch(error){
+    console.error("Error adding book:", error);
+    res.status(500).send("Internal Server Error");
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
